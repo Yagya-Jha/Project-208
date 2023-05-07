6d89dc41ea6f68861081f17ffd509f9e8830a439
Yagya-Jha <74773203+Yagya-Jha@users.noreply.github.com>
2023-05-07 20:23:56 +0530

    Initial commit

---
+++ client.py
@@ -0,0 +1,111 @@
+import socket
+from threading import Thread
+from tkinter import *
+from tkinter import ttk
+
+
+PORT  = 8080
+IP_ADDRESS = '127.0.0.1'
+SERVER = None
+BUFFER_SIZE = 4096
+
+
+name = None
+listbox =  None
+textarea= None
+labelchat = None
+text_message = None
+
+bgc="#490c5c"
+fgc="pink"
+bdc="#f73eba"
+
+
+def openChatWindow():
+
+    global bgc
+    global fgc
+    global bdc
+    print("\n\t\t\t\tIP Music Sharing App")
+
+    window=Tk()
+
+    window.title('Music Sharing App')
+    window.geometry("500x350")
+    window.configure(bg=bgc)
+
+    global name
+    global listbox
+    global textarea
+    global labelchat
+    global text_message
+    global file_path_label
+
+    namelabel = Label(window, text= "Enter Your Name", font = ("Calibri",10), bg=fgc, highlightthickness=2, highlightbackground=bdc)
+    namelabel.place(x=10, y=8)
+
+    name = Entry(window,width =30,font = ("Calibri",10), bg=fgc)
+    name.place(x=120,y=8)
+    name.focus()
+
+    connectserver = Button(window,text="Connect to Chat Server",bd=1, font = ("Calibri",10), bg=fgc)
+    connectserver.place(x=350,y=6)
+
+    separator = ttk.Separator(window, orient='horizontal')
+    separator.place(x=0, y=35, relwidth=1, height=0.1)
+
+    labelusers = Label(window, text= "Active Users", font = ("Calibri",10), bg=fgc, highlightthickness=2, highlightbackground=bdc)
+    labelusers.place(x=10, y=50)
+
+    listbox = Listbox(window,height = 5,width = 67,activestyle = 'dotbox', font = ("Calibri",10), bg=fgc, highlightthickness=2, highlightbackground=bdc)
+    listbox.place(x=10, y=70)
+
+    scrollbar1 = Scrollbar(listbox)
+    scrollbar1.place(relheight = 1,relx = 1)
+    scrollbar1.config(command = listbox.yview)
+
+    connectButton=Button(window,text="Connect",bd=1, font = ("Calibri",10), bg=fgc)
+    connectButton.place(x=282,y=160)
+
+    disconnectButton=Button(window,text="Disconnect",bd=1, font = ("Calibri",10), bg=fgc)
+    disconnectButton.place(x=350,y=160)
+
+    refresh=Button(window,text="Refresh",bd=1, font = ("Calibri",10), bg=fgc)
+    refresh.place(x=435,y=160)
+
+    labelchat = Label(window, text="Chat Window", font=("Calibri",10), bg=fgc, highlightthickness=2, highlightbackground=bdc)
+    labelchat.place(x=10,y=180)
+  
+    textarea = Text(window, width=67, height=6, font=("Calibri",10), bg=fgc, highlightthickness=2, highlightbackground=bdc)
+    textarea.place(x=10,y=200)
+    
+    scrollbar2 = Scrollbar(textarea)
+    scrollbar2.place(relheight = 1,relx = 1)
+    scrollbar2.config(command = textarea.yview)
+
+    text_message = Entry(window, width=45, font=("Calibri", 10), bg=fgc)
+    text_message.pack()
+    text_message.place(x=98, y=300)
+    
+    attach = Button(window, text = "Attach and Send", bd = 1, font=("Calibri", 10), bg=fgc)
+    attach.place(x=10,y=300)
+
+    file_path_label = Label(window, text="", fg="blue",font=("Calibri", 10), bg=fgc)
+    file_path_label.place(x=10,y=300)
+
+    snd_bttn = Button(window, text="Send",font=("Calibri", 10), bg=fgc)
+    snd_bttn.place(x=430, y=300)
+  
+    window.mainloop()
+
+def setup():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS, PORT))
+   
+    openChatWindow()
+
+setup()
---
+++ server.py
@@ -0,0 +1,48 @@
+import socket
+from  threading import Thread
+import time
+
+IP_ADDRESS = '127.0.0.1'
+PORT = 8080
+SERVER = None
+clients = {}
+
+def acceptConnections():
+    global SERVER
+    global clients
+
+    while True:
+        client, addr = SERVER.accept()
+
+        client_name = client.recv(4096).decode().lower()
+        clients[client_name] = {
+                "client"         : client,
+                "address"        : addr,
+                "connected_with" : "",
+                "file_name"      : "",
+                "file_size"      : 4096
+            }
+
+        print(f"Connection established with {client_name} : {addr}")
+
+
+def setup():
+    print("\n\t\t\t\t\t\tMusic Sharing App\n")
+
+    global PORT
+    global IP_ADDRESS
+    global SERVER
+
+
+    SERVER  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS, PORT))
+    SERVER.listen(100)
+
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...")
+    print("\n")
+
+    acceptConnections()
+
+
+setup_thread = Thread(target=setup)
+setup_thread.start()
